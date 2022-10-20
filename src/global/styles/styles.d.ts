ab6bf2bfad32b21c99a07814847ed59d2aa4e5b7
mateusdegobi <kim3dis@gmail.com>
2022-10-20 07:21:11 -0300

    chore: fix main theme typing

--- src/global/styles/styles.d.ts
+++ src/global/styles/styles.d.ts
@@ -2,7 +2,8 @@ import 'styled-components/native';
 import theme from './theme';
 
 declare module 'styled-components' {
-  export interface DefaultTheme{
-    main: typeof theme;
+  export interface DefaultTheme {
+    colors: typeof theme.colors;
+    fonts: typeof theme.fonts;
   }
 }
