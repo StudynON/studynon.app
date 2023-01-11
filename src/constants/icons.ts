a000f51eeab4ce3c9730e4f5518c2025f50e00d6
mateus degobi <kim3dis@gmail.com>
2023-01-11 20:10:51 -0300

    chore: add icons

--- src/constants/icons.ts
+++ src/constants/icons.ts
@@ -1,7 +1,20 @@
 import iconMenu from '../assets/icons/icon-menu.png';
+import plusHome from '../assets/icons/plus-home-button.png';
+import listHome from '../assets/icons/list-home-button.png';
+import goalHome from '../assets/icons/goal-home-button.png';
+import calendarHome from '../assets/icons/calendar-home-button.png';
 
-export type NameIconsTypes = 'iconMenu';
+export type NameIconsTypes =
+  | 'iconMenu'
+  | 'plusHome'
+  | 'listHome'
+  | 'goalHome'
+  | 'calendarHome';
 
 export default {
   iconMenu,
+  plusHome,
+  listHome,
+  goalHome,
+  calendarHome,
 };
